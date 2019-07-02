1ec9d6a37ec0c455c442516485b13d384bf1cd41
Aman <im.developer26@gmail.com>
2019-07-02 23:42:40 +0530

    Initial Commit

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HelloWorld from "./components/HelloWorld";
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode: history,
+    routes:[
+        {
+            path: '/',
+            component:HelloWorld
+        }
+    ]
+
+})
+
